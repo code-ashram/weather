b308950b32da665875a95d25c5ddb1be3ca4c618
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-03-01 18:50:40 +0300

    add jquery slider

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,30 @@ import {getWeatherData} from './api'
 
 getWeatherData().then(result => console.log(result))
 
-const carousel = new Carousel('#forecastSlider')
+const myCarouselElement = new Carousel('#forecastSlider')
+
+let carouselWidth = $('.carousel-inner')[0].scrollWidth
+let cardWidth = $('.carousel-item').width()
+
+let scrollPosition = 0
+
+$('.carousel').carousel({
+  touch: true // default
+})
+$('.carousel-control-next').on('click', function() {
+  if(scrollPosition < carouselWidth - (cardWidth * 5)) {
+    console.log('next')
+    scrollPosition = scrollPosition + cardWidth
+    $('.carousel-inner').animate({scrollLeft: scrollPosition}, 300)
+  }
+})
+
+$('.carousel-control-prev').on('click', function() {
+  if(scrollPosition > 0) {
+    console.log('prev')
+    scrollPosition = scrollPosition - cardWidth
+    $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600)
+  }
+})
+
 
