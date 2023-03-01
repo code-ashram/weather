c8c612ce10c7edbb1c3d44a8eefb6324f8fd47c2
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-03-01 17:53:11 +0300

    add cards for slider

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 // @ts-ignore
 import * as bootstrap from 'bootstrap'
 import './style.scss'
-
+import { Carousel } from 'bootstrap'
 import {getWeatherData} from './api'
 
+
+
 getWeatherData().then(result => console.log(result))
+
+const carousel = new Carousel('#forecastSlider')
+
