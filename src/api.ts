54f4f25184b2d632d0b82d09fa82dc9af9bb2b2f
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-03-02 21:41:05 +0300

    finish first GUI

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import {Weather} from './types'
 
-export const getWeatherData = async ():Promise<Weather> => fetch('https://api.open-meteo.com/v1/forecast?latitude=52.43&longitude=30.98&hourly=temperature_2m,showers,visibility,windspeed_10m&windspeed_unit=ms')
+export const getWeatherData = async ():Promise<Weather> => fetch('https://api.open-meteo.com/v1/forecast?latitude=52.43&longitude=30.98&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure,visibility,windspeed_10m,winddirection_80m')
   .then(response => response.json())
 
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,10 @@
 // @ts-ignore
 import * as bootstrap from 'bootstrap'
 import './style.scss'
-import { Carousel } from 'bootstrap'
 import {getWeatherData} from './api'
-
-
 
 getWeatherData().then(result => console.log(result))
 
-const myCarouselElement = new Carousel('#forecastSlider')
 
-let carouselWidth = $('.carousel-inner')[0].scrollWidth
-let cardWidth = $('.carousel-item').width()
-
-let scrollPosition = 0
-
-$('.carousel').carousel({
-  touch: true // default
-})
-$('.carousel-control-next').on('click', function() {
-  if(scrollPosition < carouselWidth - (cardWidth * 5)) {
-    console.log('next')
-    scrollPosition = scrollPosition + cardWidth
-    $('.carousel-inner').animate({scrollLeft: scrollPosition}, 300)
-  }
-})
-
-$('.carousel-control-prev').on('click', function() {
-  if(scrollPosition > 0) {
-    console.log('prev')
-    scrollPosition = scrollPosition - cardWidth
-    $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600)
-  }
-})
 
 
