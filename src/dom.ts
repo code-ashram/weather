e05fba5b7acf624108fdade6bd408335d63b48ab
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-03-13 17:50:00 +0300

    add weather info for daily forecast

--- src/dom.ts
+++ src/dom.ts
@@ -1,5 +1,6 @@
 import { getCurrentWeather, getDailyWeather } from './api'
 import { weatherStatus } from './weather-code'
+import * as bootstrap from 'bootstrap'
 
 const city = document.getElementById('city') as HTMLDivElement
 const cityName = city.querySelector('.city__name') as HTMLParagraphElement
@@ -7,6 +8,17 @@ const cityTemperature = city.querySelector('.city__current-temperature') as HTML
 const cityWeatherStatus = city.querySelector('.city__weather-status') as HTMLParagraphElement
 const cityMaxTemperature = city.querySelector('.city__max-t') as HTMLParagraphElement
 const cityMinTemperature = city.querySelector('.city__min-t') as HTMLParagraphElement
+
+const triggerTabList = document.querySelectorAll('#myTab button')
+
+triggerTabList.forEach(triggerEl => {
+  const tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', event => {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
 
 export const showCurrentWeather = () => {
 
@@ -24,4 +36,85 @@ export const showCurrentWeather = () => {
   })
 }
 
+// const date = new Date("2023-03-12");
 
+const editDate = (date: string): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    day: 'numeric',
+    month: 'short'
+  }
+  return new Date(date).toLocaleDateString('en-GB', options)
+}
+
+const createWeatherCard = (title: string, picture: string, text: string) : HTMLDivElement => {
+
+  const cardList = document.getElementById('dailyForecast') as HTMLDivElement
+
+  const card = document.createElement('div') as HTMLDivElement
+  card.classList.add('card', 'forecast__card')
+
+  const cardBody = document.createElement('div') as HTMLDivElement
+  cardBody.classList.add('card-body', 'forecast__card-body')
+
+  const cardTitle = document.createElement('h5') as HTMLHeadingElement
+  cardTitle.classList.add('card-title', 'forecast__card-title')
+  cardTitle.textContent = title
+
+  const cardImg = document.createElement('img') as HTMLImageElement
+  cardImg.classList.add('card-img-top')
+  cardImg.src = picture
+  cardImg.alt = 'weather ico'
+
+  const cardText = document.createElement('p') as HTMLParagraphElement
+  cardText.classList.add('card-text', 'forecast__card-text')
+  cardText.textContent = text
+
+  cardBody.append(cardTitle, cardImg, cardText)
+  card.append(cardBody)
+  cardList.append(card)
+
+  return card
+}
+
+const determineWeatherIco = (weatherCode: number): string => {
+  let cardImg = ''
+
+  if (weatherCode <= 9) {
+    cardImg = 'src/img/sunny.png'
+  } else if (weatherCode > 9 && weatherCode < 20) {
+    cardImg = 'src/img/sunny_intervals.png'
+  } else if (weatherCode >= 20 && weatherCode < 30) {
+    cardImg = 'src/img/cloudy_with_light_snow.png'
+  } else if (weatherCode >= 30 && weatherCode < 40) {
+    cardImg = 'src/img/wind.png'
+  } else if (weatherCode >= 40 && weatherCode < 50) {
+    cardImg = 'src/img/mist.png'
+  } else if (weatherCode >= 50 && weatherCode < 60) {
+    cardImg = 'src/img/drizzle.png'
+  } else if (weatherCode >= 60 && weatherCode < 70) {
+    cardImg = 'src/img/cloudy_with_light_rain.png'
+  } else if (weatherCode >= 70 && weatherCode < 80) {
+    cardImg = 'src/img/cloudy_with_light_snow.png'
+  } else if (weatherCode >= 80 && weatherCode < 90) {
+    cardImg = 'src/img/cloudy_with_heavy_rain.png'
+  } else if (weatherCode >= 90 && weatherCode < 99) {
+    cardImg = 'src/img/thunderstorms.png'
+  } else {
+    console.log('Error!')
+  }
+
+  return cardImg
+}
+
+export const appendWeatherCard = (): void => {
+  getDailyWeather().then((response) => {
+    for (let i = 0; i < response.daily.time.length; i++) {
+
+      const cardTitle = editDate(response.daily.time[i])
+      const cardImg = determineWeatherIco(response.daily.weathercode[i])
+      const cardText = `${Math.round((response.daily.temperature_2m_max[i] + response.daily.temperature_2m_min[i]) / 2)}`
+
+      createWeatherCard(cardTitle, cardImg, cardText)
+    }
+  })
+}
--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,14 @@
 import * as bootstrap from 'bootstrap'
 import './style.scss'
 import { getCurrentWeather, getDailyWeather, getHourlyWeather } from './api'
-import { showCurrentWeather } from './dom'
+import { appendWeatherCard, showCurrentWeather } from './dom'
 
 
 getHourlyWeather().then(result => console.log(result))
 getDailyWeather().then(result => console.log(result))
 getCurrentWeather().then(result => console.log(result))
 
-const triggerTabList = document.querySelectorAll('#myTab button')
 
-triggerTabList.forEach(triggerEl => {
-  const tabTrigger = new bootstrap.Tab(triggerEl)
-
-  triggerEl.addEventListener('click', event => {
-    event.preventDefault()
-    tabTrigger.show()
-  })
-})
 
 showCurrentWeather()
-
-const date = new Date("2023-03-12");
-
-const options: Intl.DateTimeFormatOptions = {
-  day: 'numeric',
-  month: 'short',
-}
-
-const string = date.toLocaleDateString('en-GB', options)
-
-console.log(string)
+appendWeatherCard()
